84a70aec14f8096cd182422611798db1f322906f
yogesh357 <ypdhakane101@gmail.com>
2026-02-08 15:53:27 +0530

    fix

--- backend/src/types/type.ts
+++ backend/src/types/type.ts
@@ -4,9 +4,11 @@ export const createTaskInputs = z.object({
     options: z.array(z.object({
         imageUrl: z.string()
     })),
+
     title: z.string().optional(),
     signature: z.string()
 })
+
 
 export const createSubmissionInput = z.object({
     taskId: z.string(),
